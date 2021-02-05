44d596bf6f5bd38e648f9f74408f99f3ecef9073
EC2 Default User <ec2-user@ip-172-31-45-202.us-east-2.compute.internal>
2021-02-05 21:44:52 +0000

    No hand holding attempt to commit

---
+++ Introduction_to_Programming_with_Javascript/The_Basics/Exercises.js
@@ -0,0 +1,5 @@
+let concat = function (firstString, secondString) {
+  let newString = firstString + " " + secondString;
+}//Combines two strings with a space in the middle
+
+console.log(concat("John", "Doe"));
