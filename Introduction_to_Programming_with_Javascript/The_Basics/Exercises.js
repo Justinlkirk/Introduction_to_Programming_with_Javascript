e6c6f8c1731f1a99e82aa0cf73eae9ccc75dc028
EC2 Default User <ec2-user@ip-172-31-45-202.us-east-2.compute.internal>
2021-02-05 22:49:59 +0000

    Finished the first chapter of the book

--- Introduction_to_Programming_with_Javascript/The_Basics/Exercises.js
+++ Introduction_to_Programming_with_Javascript/The_Basics/Exercises.js
@@ -21,6 +21,19 @@ let exerciseTwo = function (number) {
   return "The number had more than 100 digits or something failed!";
 }//Uses arithmatic operators to determine each digit in given number
 
+let exerciseSix = function (numberOne, numberTwo) {
+  return `The value of ${numberOne} plus ${numberTwo} is ${numberOne + numberTwo}.`
+}//Adds two numbers together and logs the process.
+
+let exerciseEight = function (...names) {
+  return names;
+}
+
+let exerciseNine = function () {
+  let petObject = { Asta: 'dog', Butterscotch: 'cat', Pudding: 'cat', Neptune: 'fish', Darwin: 'lizard' };
+  return petObject;
+}//Creates an object with the keys being the name of the pet and the value being the type of pet it is
+
 console.log(exerciseOne("John", "Doe"));//John Doe
 
 console.log(exerciseTwo(4936));/*The digits of your number from the ones place onward are:
@@ -29,4 +42,18 @@ console.log(exerciseTwo(4936));/*The digits of your number from the ones place o
                                 3
                                 6*/
 
-console.log();
+console.log("The data type of 'true' is String, false is Boolean, 1.5 is Number, 2 is Number, undefined is Undefined, and { foo: 'bar' } is an Object.");
+
+console.log("The reason that console.log('5' + 10) outputs '510' instead of 15 is that JavaScript assumes you want 10 to be a String instead of a Number so it converts it to a string and then concatenates it with '5'.");
+
+console.log(exerciseSix(Number('5'), 10));//The value of 5 + 10 is 15.
+
+console.log("No it will output 'undefined' as that is the value currently stored there in the array.");
+
+console.log(exerciseEight("Asta", "Butterschotch", "Pudding", "Neptune", "Darwin"));// [Asta, Butterscotch, Pudding, Neptune, Darwin]
+
+console.log(exerciseNine());//{ Asta: 'dog', Butterscotch: 'cat', Pudding: 'cat', Neptune: 'fish', Darwin: 'lizard' }
+
+console.log("parseInt('3.1415') evaluates to 3 because when it hits the decimal it stops and returns all the number it saw before it. If the number was not in quotes it would return the entire number");//The statement thats in there.
+
+console.log("'12' < '9' evaluates as true. This makes sense because 'a' < 'b' returns true as JavaScript attempts to convert each string index to a number and compare. Therefore it compares 1 < 9 then 2 < 9");//The statement thats in there.
