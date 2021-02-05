07f71b13215a523f32b1f7df4dbce13c77153d8f
EC2 Default User <ec2-user@ip-172-31-45-202.us-east-2.compute.internal>
2021-02-05 22:18:33 +0000

    Finished a few exercises

--- Introduction_to_Programming_with_Javascript/The_Basics/Exercises.js
+++ Introduction_to_Programming_with_Javascript/The_Basics/Exercises.js
@@ -1,5 +1,32 @@
-let concat = function (firstString, secondString) {
+let exerciseOne = function (firstString, secondString) {
   let newString = firstString + " " + secondString;
+  return newString;
 }//Combines two strings with a space in the middle
 
-console.log(concat("John", "Doe"));
+let exerciseTwo = function (number) {
+  let digit, newNumber = number, count = 0;
+  
+  console.log("The digits of your number from the ones place onward are:");
+  
+  do {
+    digit = newNumber % 10;
+    console.log(digit);
+    
+    newNumber /= 10;
+    newNumber -= digit/10;
+    if (newNumber < 10) return newNumber;
+    
+    count++;
+  } while (count < 100);
+  return "The number had more than 100 digits or something failed!";
+}//Uses arithmatic operators to determine each digit in given number
+
+console.log(exerciseOne("John", "Doe"));//John Doe
+
+console.log(exerciseTwo(4936));/*The digits of your number from the ones place onward are:
+                                4
+                                9
+                                3
+                                6*/
+
+console.log();
